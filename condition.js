caa9bea70fd7666d2ecf9be43806bb5a54aa9388
unknown <miliegainon@hotmail.fr>
2025-10-07 14:06:03 +0200

    save

---
+++ condition.js
@@ -0,0 +1,19 @@
+let age = 18;
+let age2 = 3;
+
+if (age >= 18) {
+  console.log("Tu es majeur.");
+} else {
+  console.log("Tu es mineur.");
+}
+
+let micro = '🎤';
+
+if (true) {
+let musique ='🎶';
+console.log(micro);
+console.log(musique);
+}
+
+console.log(micro);
+console.log(musique);
